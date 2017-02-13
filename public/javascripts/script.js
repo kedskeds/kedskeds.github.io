9deb0283550162e210677c87065d3f3424bcb45a
Kelsey Edelstein <kelseyedelstein@gmail.com>
2017-02-12 16:07:37 -0800

    Modify media queries for mobile devices

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -6,4 +6,12 @@ $(document).ready(function(){
       $( this ).removeClass( "shadow" );
     }
   );
+  $(".expand").click(function(){
+    this.expand = !this.expand;
+    $(this).text(this.expand?"Read Less":"Read More");
+    $(this).next(".desktop").toggle(200,"swing");
+  });
+  $(".project-heading").click(function(){
+    $(this).parent().find("img").toggle(200, "swing");
+  });
 });
