9f2880adf94af0083ac685851daf165333541af0
Kelsey Edelstein <kelseyedelstein@gmail.com>
2018-07-09 08:36:10 -0700

    Init new portfolio

--- app.js
+++ app.js
@@ -20,11 +20,18 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(require('stylus').middleware(path.join(__dirname, 'public')));
+// app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+
+
+require('./lib/compileSass').compileSassLibs().catch(console.error);
+
+// Also include the CSS routes for on-the-fly compiling
+const css = require('./routes/css');
+app.use('/css', css);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,11 +1,8 @@
 $(document).ready(function(){
-  $(".expand").click(function(){
-    this.expand = !this.expand;
-    $(this).text(this.expand?"Got it":"Tell Me More");
-    $(this).next(".desktop").toggle(200, "swing");
-  });
-  $(".project-heading").click(function(){
-    var img = $(this).parent().find("img");
-    img.toggle();
-  });
+  $("body").addClass("loaded");
+  $("#about .content").addClass("loaded");
+  setTimeout(function(){
+    $("nav").addClass("loaded");
+    
+  }, 1000);
 });
