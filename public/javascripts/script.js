b32e510f7adb209ed3f01688345f3a842a1c26a1
Kelsey Edelstein <kelseyedelstein@gmail.com>
2019-01-21 21:47:49 -0800

    Remove old files

--- public/javascripts/script.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-  $("body").addClass("loaded");
-  $("#about .content").addClass("loaded");
-  setTimeout(function(){
-    $("nav").addClass("loaded");
-    
-  }, 1000);
-});
