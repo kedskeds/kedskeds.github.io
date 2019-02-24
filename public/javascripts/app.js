4a574b79775203c11590864cb23df85f1fabdb42
Kelsey Edelstein <kelseyedelstein@gmail.com>
2019-02-24 14:10:23 -0800

    Portfolio refresh

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,29 +1,51 @@
+var widths = ['100px', '50px', '80px', '20px', '30px']
+var colorClasses = ['c-b', 'c-g', 'c-o', 'c-y', 'c-p', 'spacer', 'spacer', 'spacer', 'spacer', 'spacer']
+
+function getRand(min, max) {
+    return Math.floor(Math.random() * ((max + 1) - min)) + min
+}
+
+function randArrVal(arr) {
+  return arr[getRand(0, arr.length - 1)]
+}
+
+
+function writeCode() {
+	var code = $(".screen .code")
+	for(var i = 0; i < code.length; i++) {
+	  $(code[i]).css('width', randArrVal(widths))
+	  $(code[i]).addClass(randArrVal(colorClasses))
+	}
+}
+
+function revealLines() {
+	var lines = $(".line")
+
+	for(var i = 0; i < lines.length; i++) {
+	  // if(i % 5 !== 0) {
+	    $(lines[i]).addClass('line-reveal')
+	  // }
+	}
+}
+
+function animateIn() {
+  $(".intro--text").addClass("fade-right");
+  $(".editor").addClass("zoom-in");
+}
+
+
 $(document).ready(function() {
-  var menuItems = $(".header nav li")
+  animateIn();
 
-  for(var i = 0; i < menuItems.length; i++) {
-    console.log(menuItems[i])
-  }
-  
-  var sections = $('section')
-    , nav = $('nav')
-    , nav_height = nav.outerHeight();
-   
-  $(window).on('scroll', function () {
-    var cur_pos = $(this).scrollTop();
-   
-    sections.each(function() {
-      var top = $(this).offset().top - nav_height,
-          bottom = top + $(this).outerHeight();
-   
-      if (cur_pos >= top && cur_pos <= bottom) {
-        nav.find('a').removeClass('active');
-        sections.removeClass('active');
-   
-        $(this).addClass('active');
-        nav.find('a[href="#'+$(this).attr('id')+'"]').addClass('active');
-      }
-    });
-  });
+  writeCode();
+  revealLines();
 
+  var rellax = new Rellax('.rellax');
 })
+
+ $(function() {
+  $('.scroll-btn').click (function() {
+    $('html, body').animate({scrollTop: $('section#about').offset().top }, 'slow');
+    return false;
+  });
+});
