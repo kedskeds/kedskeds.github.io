9e5d7bdd43a78ac0d7ea81ca11d138bee0e75dd3
Kelsey Edelstein <kelseyedelstein@gmail.com>
2019-08-07 08:44:21 -0700

    Move project data to render method

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,69 @@
 var express = require('express');
 var router = express.Router();
 
+var projects = [
+  {
+    id:0,
+    name:"Muse / Custom Theme",
+    link:"https://www.itsyourmusebg.com",
+    title:"Muse",
+    lang:"Rails, HAML, SCSS, JQuery, Foundation, Lazyloading, Google Maps API, AOS",
+    features:"I built this website theme to be entirely CMS-editable, down to the color palette.",
+    image:"url(../images/muse.png)"
+  },
+  {
+    id:1,
+    name:"GOA / Search Tool",
+    link:"ttps://globalonlineacademy.org/what-we-do/student-program/student-courses",
+    title:"Global Online Academy",
+    lang:"Vue.JS",
+    features:"Using data from a course catalog API, I built a reactive search tool that enables students to quickly sort and filter a list of classes offered.",
+    image:"url(../images/goa.png)"
+  },
+  {
+    id:2,
+    name:"Vestalia / Custom Site",
+    link:"https://www.vestaliaglendale.com/vestalia-glendale-ca/",
+    title:"Lincoln - Vestalia",
+    lang:"Rails, HAML, SCSS, JQuery, Foundation, ScrollMagic, SVG animations",
+    features:"CMS-editable website built for a luxury apartment complex in Los Angeles.",
+    image:"url(../images/vestalia.png)"
+  },
+  {
+    id:3,
+    name:"Sutter Green / Custom Site",
+    link:"https://www.suttergreenapts.net/sutter-green-sacramento-ca/",
+    title:"Sutter Green",
+    lang:"Rails, HAML, SCSS, JQuery, Foundation, ScrollMagic, AOS",
+    features:"",
+    image:"url(../images/sutter-green.png)"
+  },
+  {
+    id:4,
+    name:"LPC / Infographic",
+    link:"https://www.lincolnapts.com/lpc-live-inspired/live-your-bliss",
+    title:"Lincoln Property Company",
+    lang:"HAML, SCSS, JQuery, ScrollMagic, Greensock, SVG animations",
+    features:"Every quarter, Lincoln Property Company puts out an animated, scrolling infographic. I developed the Q3 and Q4 infographics for 2018.",
+    image:"url(../images/lincoln.png)"
+  },
+  {
+    id:5,
+    name:"CWS Students / Custom Theme",
+    link:"https://www.cwsapartments.com/regents-west-at-24th-austin-tx/",
+    title:"Regents West",
+    lang:"Rails, HAML, SCSS, JQuery, Foundation",
+    features:"CMS-editable theme for student housing in Austin, TX.",
+    image:"url(../images/students.png)"
+  }
+];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Kelsey Edelstein | Web Developer' });
+  res.render('index', {
+    title: 'Kelsey Edelstein | Web Developer',
+    projects: projects
+  });
 });
 
 module.exports = router;
